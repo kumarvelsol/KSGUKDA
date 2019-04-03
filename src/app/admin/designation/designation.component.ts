a2a6e1350075286db0b225c24c6dab75ba213b73
Aneesha77 <40847659+Aneesha77@users.noreply.github.com>
2019-04-03 10:21:02 +0530

    changes

--- src/app/admin/designation/designation.component.ts
+++ src/app/admin/designation/designation.component.ts
@@ -41,7 +41,7 @@ export class DesignationComponent implements OnInit {
   displayedColumns: string[] = ['designation_id','designation_code','designation_name'];
   //departmant_name : this.departmant_name;
   constructor(private designationservice: AdminServiceService) { }
-
+  public deptnames: Department[];
   ngOnInit() {
    this.designationservice.getdesignation().subscribe((data: DesignationList) => 
    {
@@ -49,14 +49,15 @@ export class DesignationComponent implements OnInit {
      console.log(this.designationlist);
      this.abDatasource = new MatTableDataSource(this.designationlist.Data);
     });
-    this.designationservice.getdepartment().subscribe((dataa : DepartmentList) => 
+    this.designationservice.getdepartment().subscribe((dataa : DepartmentList ) =>
     {
-      this.deptlist = dataa;
-      console.log(this.deptlist);
-      this.abDatasource = new MatTableDataSource(this.deptlist.Data);
+     // this.deptnames = dataa;
+      // this.deptlist = dataa;
+      // console.log(this.deptlist);
+      // this.abDatasource = new MatTableDataSource(this.deptlist.Data);
     });
   }
-
+  selected = null;
   buttoncontent:string = 'Save';
   designation_id : number ;
   designation_code :string =''
